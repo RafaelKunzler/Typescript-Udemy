45c5e7ea62583265be3dd66767a1ba0b81bbbb55
RafaelKunzler <rafaelkunzler@gmail.com>
2024-07-01 14:14:05 -0300

    tipos

---
+++ 2_FUNDAMENTOS/index.js
@@ -0,0 +1,23 @@
+// 1 - numbers
+var x = 10;
+console.log(x);
+x = 16;
+console.log(typeof x);
+var y = 15.42342;
+console.log(typeof y);
+console.log(y);
+console.log(y.toPrecision(3));
+// 2 - string
+var firstName = "Rafael";
+console.log(firstName.toUpperCase());
+var fullName;
+var lastName = "Rodrigues";
+fullName = firstName + " " + lastName;
+console.log(fullName);
+console.log(typeof fullName);
+// 3 - boolean
+var a = false;
+console.log(a);
+console.log(typeof a);
+a = true;
+console.log(a);
